9f86822f05fcd42bfa6572572a5fd0b0a9b95b12
franneck94 <schaffranek.jan@web.de>
2021-11-01 11:42:38 +0100

    up example

--- 1_Basics/TimeConverter/main.cc
+++ 1_Basics/TimeConverter/main.cc
@@ -4,6 +4,10 @@ float convertMillisecondsToSeconds(float);
 float convertSecondsToMinutes(float);
 float convertMinutesToHours(float);
 
+float convertMillisecondsToMinutes(float);
+float convertMillisecondsToHours(float);
+float convertSecondsToHours(float);
+
 int main()
 {
     float InputMilliseconds = 0.0F;
@@ -15,9 +19,17 @@ int main()
     float Minutes = convertSecondsToMinutes(Seconds);
     float Hours = convertMinutesToHours(Minutes);
 
-    std::cout << Seconds << std::endl;
-    std::cout << Minutes << std::endl;
-    std::cout << Hours << std::endl;
+    float Minutes2 = convertMillisecondsToMinutes(InputMilliseconds);
+    float Hours2 = convertMillisecondsToHours(InputMilliseconds);
+    float Hours3 = convertSecondsToHours(Seconds);
+
+    std::cout << "Seconds: " << Seconds << std::endl;
+    std::cout << "Minutes: " << Minutes << std::endl;
+    std::cout << "Hours: " << Hours << std::endl;
+
+    std::cout << "Minutes2: " << Minutes2 << std::endl;
+    std::cout << "Hours2: " << Hours2 << std::endl;
+    std::cout << "Hours3: " << Hours3 << std::endl;
 
     return 0;
 }
@@ -35,4 +47,30 @@ float convertSecondsToMinutes(float Seconds)
 float convertMinutesToHours(float Minutes)
 {
     return Minutes / 60.0F;
+}
+
+float convertMillisecondsToMinutes(float Millisconds)
+{
+    float Seconds = convertMillisecondsToSeconds(Millisconds);
+    float Minutes = convertSecondsToMinutes(Seconds);
+
+    return Minutes;
+}
+
+float convertMillisecondsToHours(float Millisconds)
+{
+    float Seconds = convertMillisecondsToSeconds(Millisconds);
+    float Minutes = convertSecondsToMinutes(Seconds);
+    float Hours = convertMinutesToHours(Minutes);
+
+    return Hours;
+}
+
+
+float convertSecondsToHours(float Seconds)
+{
+    float Minutes = convertSecondsToMinutes(Seconds);
+    float Hours = convertMinutesToHours(Minutes);
+
+    return Hours;
 }
