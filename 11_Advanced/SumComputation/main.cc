a287c7facbf1ce76b12d036506db0acc8c3ff69a
franneck94 <schafranek.jan@web.de>
2021-11-06 12:36:31 +0100

    up

--- 11_Advanced/SumComputation/main.cc
+++ 11_Advanced/SumComputation/main.cc
@@ -27,8 +27,8 @@ int parallel_sum_omp(std::vector<int> &vec)
 {
     int final_sum = 0;
     int sum = 0;
-    int i = 0;
-    int n = static_cast<int>(vec.size());
+    unsigned int i = 0;
+    unsigned int n = static_cast<unsigned int>(vec.size());
 
 #pragma omp parallel for reduction(+ : sum) num_threads(NUM_THREADS)
     for (i = 0; i < n; ++i)
@@ -68,7 +68,7 @@ void range_sum_thread(RandomIter start, RandomIter stop, int &sum)
 
 int main()
 {
-    std::vector<int> my_vector(20'000'000, 0);
+    std::vector<int> my_vector(30'000'000, 0);
     random_vector(my_vector);
 
     double time1 = 0.0;
@@ -79,7 +79,7 @@ int main()
         sum1 = range_sum_asyn(my_vector.begin(), my_vector.end());
         time1 += t1.elapsed_time<cpptiming::millisecs, double>();
     }
-    std::cout << "Mean Time1: " << time1 / NUM_RUNS << "ms Sum: " << sum1 << std::endl;
+    std::cout << "Mean Async: " << time1 / NUM_RUNS << "ms Sum: " << sum1 << std::endl;
 
     double time2 = 0.0;
     volatile auto sum2 = 0;
@@ -89,7 +89,7 @@ int main()
         sum2 = std::accumulate(my_vector.begin(), my_vector.end(), 0);
         time2 += t2.elapsed_time<cpptiming::millisecs, double>();
     }
-    std::cout << "Mean Time2: " << time2 / NUM_RUNS << "ms Sum: " << sum2 << std::endl;
+    std::cout << "Mean Serial: " << time2 / NUM_RUNS << "ms Sum: " << sum2 << std::endl;
 
     double time3 = 0.0;
     volatile auto sum3 = 0;
@@ -99,19 +99,27 @@ int main()
         sum3 = parallel_sum_omp(my_vector);
         time3 += t3.elapsed_time<cpptiming::millisecs, double>();
     }
-    std::cout << "Mean Time3: " << time3 / NUM_RUNS << "ms Sum: " << sum3 << std::endl;
+    std::cout << "Mean OpenMP: " << time3 / NUM_RUNS << "ms Sum: " << sum3 << std::endl;
 
     auto sum4 = 0;
     auto length = my_vector.size();
-    auto mid = my_vector.begin() + length / 2;
+    auto quarter = static_cast<long>(length) / 4L;
+    auto half = static_cast<long>(length) / 2L;
+    auto split1 = my_vector.begin() + quarter;
+    auto split2 = my_vector.begin() + half;
+    auto split3 = my_vector.begin() + half + quarter;
     double time4 = 0.0;
     cpptiming::Timer t4;
-    std::thread t1(range_sum_thread<decltype(my_vector)::iterator>, my_vector.begin(), mid, std::ref(sum4));
-    std::thread t2(range_sum_thread<decltype(my_vector)::iterator>, mid, my_vector.end(), std::ref(sum4));
-    t1.join();
-    t2.join();
+    std::thread thread1(range_sum_thread<decltype(my_vector)::iterator>, my_vector.begin(), split1, std::ref(sum4));
+    std::thread thread2(range_sum_thread<decltype(my_vector)::iterator>, split1, split2, std::ref(sum4));
+    std::thread thread3(range_sum_thread<decltype(my_vector)::iterator>, split2, split3, std::ref(sum4));
+    std::thread thread4(range_sum_thread<decltype(my_vector)::iterator>, split3, my_vector.end(), std::ref(sum4));
+    thread1.join();
+    thread2.join();
+    thread3.join();
+    thread4.join();
     time4 += t4.elapsed_time<cpptiming::millisecs, double>();
-    std::cout << "Mean Time3: " << time4 / 1 << "ms Sum: " << sum4 << std::endl;
+    std::cout << "Mean Threadding: " << time4 / 1 << "ms Sum: " << sum4 << std::endl;
 
     return 0;
 }
