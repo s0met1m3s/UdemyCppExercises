7a2723111ef14cae45bc5a0ed425b23255dd3130
franneck94 <schafranek.jan@web.de>
2021-11-18 10:26:28 +0100

    utils on windowe broklen

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -102,14 +102,10 @@ void random_vector(std::vector<T> &vec)
 
 void clear_console()
 {
-#if defined _WIN32
+#if defined(__LINUX__) || defined(__gnu_linux__) || defined(__linux__) || defined(__APPLE__)
+    system("clear");
+#elif defined(_WIN32)
     system("cls");
-    //clrscr(); // including header file : conio.h
-#elif defined(__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-    system("clear");
-    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences
-#elif defined(__APPLE__)
-    system("clear");
 #endif
 }
 
