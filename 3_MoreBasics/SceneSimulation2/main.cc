585630cd79eee43a0492a1b0c7ee22311adaa3fb
franneck94 <schaffranek.jan@web.de>
2022-02-03 16:52:53 +0100

    up

---
+++ 3_MoreBasics/SceneSimulation2/main.cc
@@ -0,0 +1,41 @@
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <numeric>
+#include <thread>
+
+#include "AdFunctions.hpp"
+#include "AdTypes.hpp"
+
+#include "utils.hpp"
+
+int main()
+{
+    VehicleType ego_vehicle{};
+    NeighborVehiclesType vehicles{};
+
+    init_ego_vehicle(ego_vehicle);
+    init_vehicles(vehicles);
+
+    print_vehicle(ego_vehicle);
+    print_neighbor_vehicles(vehicles);
+
+    std::cout << "Start simulation?: ";
+    char Input;
+    std::cin >> Input;
+
+    while (true)
+    {
+        clear_console();
+
+        compute_future_state(ego_vehicle, vehicles, 0.100F);
+        print_scene(ego_vehicle, vehicles);
+
+        const VehicleType *ego_lane_vehicles = get_vehicle_array(ego_vehicle.lane, vehicles);
+        longitudinal_control(ego_lane_vehicles[0], ego_vehicle);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    return 0;
+}
