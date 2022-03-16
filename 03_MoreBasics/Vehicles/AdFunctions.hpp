5a75e681a061b500c5613e1dde76aa19bd3996c3
franneck94 <schaffranek.jan@web.de>
2022-03-16 15:33:50 +0100

    up

---
+++ 03_MoreBasics/Vehicles/AdFunctions.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "AdTypes.hpp"
+
+float kph_to_mps(const float kph);
+
+void init_ego_vehicle(VehicleType &ego_vehicle);
+
+void init_vehicles(NeighborVehiclesType &vehicles);
+
+void print_vehicle(const VehicleType &vehicle);
+
+void print_neighbor_vehicles(const NeighborVehiclesType &vehicles);
+
+void compute_future_state(const VehicleType &ego_vehicle,
+                          NeighborVehiclesType &vehicles,
+                          const float seconds);
