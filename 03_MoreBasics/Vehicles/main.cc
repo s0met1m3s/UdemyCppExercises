76a0d85a42aa3050a8542eede4675d864a3d9e91
franneck94 <schaffranek.jan@web.de>
2022-03-16 16:00:15 +0100

    up

--- 03_MoreBasics/Vehicles/main.cc
+++ 03_MoreBasics/Vehicles/main.cc
@@ -16,20 +16,5 @@ int main()
     print_vehicle(ego_vehicle);
     print_neighbor_vehicles(vehicles);
 
-    print_scene(ego_vehicle, vehicles);
-
-    std::cout << "Compute forward (1sec)?: ";
-    char Input;
-    std::cin >> Input;
-
-    while (Input == 'y')
-    {
-        compute_future_state(ego_vehicle, vehicles, 1);
-        print_scene(ego_vehicle, vehicles);
-
-        std::cout << "Compute forward (1sec)?: ";
-        std::cin >> Input;
-    }
-
     return 0;
 }
