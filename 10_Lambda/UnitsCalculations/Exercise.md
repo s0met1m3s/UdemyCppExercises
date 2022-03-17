fbcef076ccb314ea00ed9d0085193df76c2e5312
franneck94 <schaffranek.jan@web.de>
2022-03-17 08:44:51 +0100

    up

--- 10_Lambda/UnitsCalculations/Exercise.md
+++ 10_Lambda/UnitsCalculations/Exercise.md
@@ -1,8 +1,63 @@
+# Exercise
+
+Implement the following functions by using std::algorithms and lambda functions (except *compute_accelerations*):
+
+```cpp
+void compute_velocities(const std::array<float, NUM_ITERATIONS> long_velocities_mps,
+                        const std::array<float, NUM_ITERATIONS> lat_velocities_mps,
+                        std::array<float, NUM_ITERATIONS> &velocities_mps);
+
+void compute_heading_degrees(const std::array<float, NUM_ITERATIONS> long_velocities_mps,
+                             const std::array<float, NUM_ITERATIONS> lat_velocities_mps,
+                             std::array<float, NUM_ITERATIONS> &heading_degrees);
+
+void compute_accelerations(const std::array<float, NUM_ITERATIONS> &velocities_mps,
+                           std::array<float, NUM_ITERATIONS> &accelerations_mps2);
+
+void compute_rel_velocities(const std::array<float, NUM_ITERATIONS> &velocities_mps,
+                            const std::array<float, NUM_ITERATIONS> &ego_velocities_mps,
+                            std::array<float, NUM_ITERATIONS> &rel_velocities_mps);
+
+void compute_rel_accelerations(const std::array<float, NUM_ITERATIONS> &accelerations_mps2,
+                               const std::array<float, NUM_ITERATIONS> &ego_accelerations_mps2,
+                               std::array<float, NUM_ITERATIONS> &rel_accelerations_mps2);
+```
+
+## Velocity
+
+To compute the general velocity we can use the lateral and long. velocity:  
+$v = \sqrt{v_{lat}^2 + v_{long}^2}$
+
+## Heading Degree
+
+To compute the heading degree we can use the following formula:  
+$d = \frac{\arctan2(v_{lat}, v_{long})}{\pi} * 180$
+
+Here is a small introduction to the vehicle heading: [link](http://street.umn.edu/VehControl/javahelp/HTML/Definition_of_Vehicle_Heading_and_Steeing_Angle.htm)  
+
+## Acceleration
+
+To estimate the acceleration we can use the current and previous velocity:  
+$v_f$: Current velocity  
+$v_i$: Previous velocity  
+$t$: Time span between those two velocities (in our case it is 50ms)  
+$a = \frac{v_{f} - v_{i}}{t}$
+
+## Relative Velocity/Acceleration
+
+This should be easy, no tips from my side ;)
+
+## Main Function
+
+```cpp
 #include <algorithm>
 #include <array>
+#include <cmath>
 
-constexpr std::size_t NUM_ITERATIONS = 3;
-constexpr std::uint32_t TIME_SPAN = 50;
+#include "Units.hpp"
+
+constexpr std::size_t NUM_ENTRIES = 3;
+constexpr std::uint32_t TIME_SPAN_MS = 50;
 
 void compute_velocities(const std::array<float, NUM_ITERATIONS> long_velocities_mps,
                         const std::array<float, NUM_ITERATIONS> lat_velocities_mps,
@@ -67,3 +122,4 @@ int main(int argc, char **argv)
     compute_rel_velocities(long_velocities_mps, ego_velocities_mps, rel_velocities_mps);
     compute_rel_accelerations(accelerations_mps2, ego_accelerations_mps2, rel_accelerations_mps2);
 }
+```
---
+++ 10_Lambda/UnitsCalculations/Main.cc
@@ -0,0 +1,106 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+
+#include "Units.hpp"
+
+constexpr std::size_t NUM_ENTRIES = 3;
+constexpr std::uint32_t TIME_SPAN_MS = 50;
+
+void compute_velocities(const std::array<float, NUM_ENTRIES> long_velocities_mps,
+                        const std::array<float, NUM_ENTRIES> lat_velocities_mps,
+                        std::array<float, NUM_ENTRIES> &velocities_mps);
+
+void compute_heading_degrees(const std::array<float, NUM_ENTRIES> long_velocities_mps,
+                             const std::array<float, NUM_ENTRIES> lat_velocities_mps,
+                             std::array<float, NUM_ENTRIES> &heading_degrees);
+
+void compute_accelerations(const std::array<float, NUM_ENTRIES> &velocities_mps,
+                           std::array<float, NUM_ENTRIES> &accelerations_mps2);
+
+void compute_rel_velocities(const std::array<float, NUM_ENTRIES> &velocities_mps,
+                            const std::array<float, NUM_ENTRIES> &ego_velocities_mps,
+                            std::array<float, NUM_ENTRIES> &rel_velocities_mps);
+
+void compute_rel_accelerations(const std::array<float, NUM_ENTRIES> &accelerations_mps2,
+                               const std::array<float, NUM_ENTRIES> &ego_accelerations_mps2,
+                               std::array<float, NUM_ENTRIES> &rel_accelerations_mps2);
+
+int main(int argc, char **argv)
+{
+    const auto ego_velocities_mps = std::array<float, NUM_ENTRIES>{25.0F, 25.6F, 26.2F};
+    const auto ego_accelerations_mps2 = std::array<float, NUM_ENTRIES>{0.0F, 0.6F, 0.6F};
+
+    const auto long_velocities_mps = std::array<float, NUM_ENTRIES>{23.0F, 25.0F, 27.0F};
+    const auto lat_velocities_mps = std::array<float, NUM_ENTRIES>{1.0F, 1.2F, 1.1F};
+
+    auto velocities_mps = std::array<float, NUM_ENTRIES>{};
+    auto heading_degrees = std::array<float, NUM_ENTRIES>{};
+    auto accelerations_mps2 = std::array<float, NUM_ENTRIES>{};
+    auto rel_velocities_mps = std::array<float, NUM_ENTRIES>{};
+    auto rel_accelerations_mps2 = std::array<float, NUM_ENTRIES>{};
+
+    compute_velocities(long_velocities_mps, lat_velocities_mps, velocities_mps);
+    compute_heading_degrees(long_velocities_mps, lat_velocities_mps, heading_degrees);
+    compute_accelerations(velocities_mps, accelerations_mps2);
+    compute_rel_velocities(long_velocities_mps, ego_velocities_mps, rel_velocities_mps);
+    compute_rel_accelerations(accelerations_mps2, ego_accelerations_mps2, rel_accelerations_mps2);
+}
+
+void compute_velocities(const std::array<float, NUM_ENTRIES> long_velocities_mps,
+                        const std::array<float, NUM_ENTRIES> lat_velocities_mps,
+                        std::array<float, NUM_ENTRIES> &velocities_mps)
+{
+    std::transform(long_velocities_mps.begin(),
+                   long_velocities_mps.end(),
+                   lat_velocities_mps.begin(),
+                   velocities_mps.begin(),
+                   [&](const auto v_long, const auto v_lat) {
+                       return std::sqrt(std::pow(v_long, 2.0F) + std::pow(v_lat, 2.0F));
+                   });
+}
+
+void compute_heading_degrees(const std::array<float, NUM_ENTRIES> long_velocities_mps,
+                             const std::array<float, NUM_ENTRIES> lat_velocities_mps,
+                             std::array<float, NUM_ENTRIES> &heading_degrees)
+{
+    std::transform(
+        long_velocities_mps.begin(),
+        long_velocities_mps.end(),
+        lat_velocities_mps.begin(),
+        heading_degrees.begin(),
+        [&](const auto v_long, const auto v_lat) { return (std::atan2(v_lat, v_long) / PI<float>)*180.0F; });
+}
+
+void compute_accelerations(const std::array<float, NUM_ENTRIES> &velocities_mps,
+                           std::array<float, NUM_ENTRIES> &accelerations_mps2)
+{
+    accelerations_mps2[0] = 0.0F;
+
+    for (std::size_t i = 1; i < NUM_ENTRIES; ++i)
+    {
+        accelerations_mps2[i] = (velocities_mps[i] - velocities_mps[i - 1]) / TIME_SPAN_MS;
+    }
+}
+
+void compute_rel_velocities(const std::array<float, NUM_ENTRIES> &velocities_mps,
+                            const std::array<float, NUM_ENTRIES> &ego_velocities_mps,
+                            std::array<float, NUM_ENTRIES> &rel_velocities_mps)
+{
+    std::transform(velocities_mps.begin(),
+                   velocities_mps.end(),
+                   ego_velocities_mps.begin(),
+                   rel_velocities_mps.begin(),
+                   [&](const auto v_veh, const auto v_ego) { return v_ego - v_veh; });
+}
+
+void compute_rel_accelerations(const std::array<float, NUM_ENTRIES> &accelerations_mps2,
+                               const std::array<float, NUM_ENTRIES> &ego_accelerations_mps2,
+                               std::array<float, NUM_ENTRIES> &rel_accelerations_mps2)
+{
+    std::transform(accelerations_mps2.begin(),
+                   accelerations_mps2.end(),
+                   ego_accelerations_mps2.begin(),
+                   rel_accelerations_mps2.begin(),
+                   [&](const auto a_veh, const auto a_ego) { return a_ego - a_veh; });
+}
