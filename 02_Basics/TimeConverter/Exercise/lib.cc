4a26b3d842aab16406c2edba9bc01937e2751e88
s0met1m3s <jenskaes@gamil.com>
2023-04-25 19:14:32 +0200

    15_04_2023

--- 02_Basics/TimeConverter/Exercise/lib.cc
+++ 02_Basics/TimeConverter/Exercise/lib.cc
@@ -4,24 +4,43 @@
 
 float convert_milliseconds_to_seconds(const float milliseconds)
 {
+   float milli_to_Sec = milliseconds / 1000;
+
+   return milli_to_Sec;
+
 }
 
 float convert_seconds_to_minutes(const float seconds)
 {
+    float Sec_to_Min = seconds / 60;
+
+    return Sec_to_Min;
 }
 
 float convert_minutes_to_hours(const float minutes)
 {
+   float min_to_hours =  minutes / 60;
+
+   return min_to_hours;
 }
 
 float convert_milliseconds_to_minutes(const float milliseconds)
 {
+   float milli_to_min =  milliseconds / 1000 / 60;
+
+   return milli_to_min;
 }
 
 float convert_milliseconds_to_hours(const float milliseconds)
 {
+    float milli_to_hours = milliseconds / 1000 / 60 / 60;
+
+    return milli_to_hours;
 }
 
 float convert_seconds_to_hours(const float seconds)
 {
+    float sec_to_hours = seconds / 60 / 60;
+
+    return sec_to_hours;
 }
